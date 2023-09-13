03626505f3ce673776d1fd196d13eb4c7fcb4677
lucashenrique22 <lh834392@gmail.com>
2023-09-12 21:48:37 -0300

    cria controller de proprietario e rotas

---
+++ app/Http/Controllers/OwnerController.php
@@ -0,0 +1,33 @@
+<?php
+//
+//namespace App\Http\Controllers;
+//
+//use App\Http\Controllers\Controller;
+//use App\Models\Owner;
+//use Illuminate\Http\Request;
+//use Illuminate\View\View;
+//
+//class OwnerController extends Controller
+//{
+//    public function index(Request $request): View
+//    {
+//        $owners = Owner::all();
+//        return view('owners.index');
+//    }
+//
+//    public function create()
+//    {
+//        return view('owners.create');
+//    }
+//
+//    public function store(Request $request)
+//    {
+//
+//    }
+//
+//    public function destroy(Owner $owner)
+//    {
+//
+//    }
+//
+//}
